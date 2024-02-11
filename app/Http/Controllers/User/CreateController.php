a8d9cb8d9f3ecae7eefb86fc90a83cca19a7ef22
Koki <wamauup.h.h.h@gmail.com>
2024-02-11 19:52:03 +0900

    ユーザー新規登録用のControllerを作成

---
+++ app/Http/Controllers/User/CreateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\CreateRequest;
+
+class CreateController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(CreateRequest $request)
+    {
+        //
+    }
+}
