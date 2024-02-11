b9cac1ca7359a330d400cd44e9f882c330e5e08b
Koki <wamauup.h.h.h@gmail.com>
2024-02-11 14:43:29 +0900

    sign-in.bladeファイルに追記して新規登録画面に必要な項目を追加

--- resources/views/user/sign-in.blade.php
+++ resources/views/user/sign-in.blade.php
@@ -5,19 +5,40 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="/css/style.css" rel="stylesheet">
     <title>sign in</title>
 </head>
 <body>
-    <h1>新規登録</h1>
-    <form action="" method="post">
-        @csrf
-        <label for="name">名前</label>
-        <input type="text" name="name" id="name">
-        <label for="email">メールアドレス</label>
-        <input type="email" name="email" id="email">
-        <label for="password">パスワード</label>
-        <input type="password" name="password" id="password">
-        <button type="submit">送信</button>
-    </form>
+    <header>
+        <div class="header-content">
+            <h1>My Portfolio</h1>
+        </div>
+        <button type="submit" class="button_white">ログイン</button>
+    </header>
+
+    <main>
+        <section class="signup-section">
+            <h2>新規登録</h2>
+            <form>
+                <div class="form-group">
+                    <label for="name">氏名</label>
+                    <input type="text" id="name">
+                </div>
+                <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input type="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">パスワード</label>
+                    <input type="password" id="password">
+                </div>
+                <button type="submit" class="submit-button">登録する</button>
+            </form>
+        </section>
+    </main>
+
+    <footer>
+        <p>portfolio site</p>
+    </footer>
 </body>
 </html>
