6779c0dfe73480ba76219b0d43f0f4f1a83392c8
Koki <wamauup.h.h.h@gmail.com>
2024-02-14 17:53:36 +0900

    新規登録ボタンの押下でsignup画面に遷移するように実装

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -29,11 +29,13 @@
                 </div>
                 @if ($errors->any())
                      <p style="color: red;">メールアドレス、もしくはパスワードが間違っています</p>
-                @endif                
-                <button type="submit" class="submit-button">ログインする</button>
+                     @endif                
+                     <button type="submit" class="submit-button">ログインする</button>
+                    </form>
+                </section>
+            <form action="{{ route('user.index') }}" method="get">
                 <button type="submit" class="submit-button">新規登録する</button>
             </form>
-        </section>
     </main>
     <footer>
         <p>portfolio site</p>
