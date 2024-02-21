75c3804b0fba36889c86e9b92b85b026459b0023
Koki <wamauup.h.h.h@gmail.com>
2024-02-21 11:48:56 +0900

    自己紹介文のバリデーションを設定

--- app/Http/Requests/User/EditRequest.php
+++ app/Http/Requests/User/EditRequest.php
@@ -11,7 +11,7 @@ class EditRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class EditRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'introduction' => [
+                'required',
+                'min:50',
+                'max:200'
+                ]
         ];
     }
 }
