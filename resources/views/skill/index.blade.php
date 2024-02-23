7f664b317ec76f696b258503d3b7bb238e2b58f7
Koki <wamauup.h.h.h@gmail.com>
2024-02-23 18:19:10 +0900

    項目一覧ページの見た目を整えた

--- resources/views/skill/index.blade.php
+++ resources/views/skill/index.blade.php
@@ -19,6 +19,28 @@
         </form>
     </header>
     <main>
+        <section class="month-section">
+            <select name="month" class="month-pulldown">
+                <option value="2" selected>2月</option>
+                <option value="1">1月</option>
+                <option value="12">12月</option>
+            </select>
+        </section>
+        <section class="category-section">
+            <h2 class="section-title">バックエンド</h2>
+            <div class="add-item-button-container">
+            <button type="button" class="submit-button">項目を追加する</button>
+            </div>
+            <div class="skills-item">
+                <label for="ruby">Ruby</label>
+                <select id="ruby" name="ruby">
+                    <option value="40" selected>40</option>
+                    <!-- 他の選択肢を追加 -->
+                </select>
+                <button type="submit" class="save-button">学習時間を保存する</button>
+                <button type="submit" class="delete-button">削除する</button>
+            </div>
+        </section>
         <section class="signup-section">
             <form action="" method="post" enctype="multipart/form-data">
                 @csrf
