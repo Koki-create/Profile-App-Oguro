58420ef3ae6c4753fbef66e7bacf8fabbfd4e25a
Koki <wamauup.h.h.h@gmail.com>
2024-02-27 15:10:12 +0900

    add.blade.phpにデバッグ用のコードを埋め込んだ

--- resources/views/data/add.blade.php
+++ resources/views/data/add.blade.php
@@ -22,7 +22,7 @@
     <main>
         <section class="signup-section">
             <h2>{{ $category }}に項目を追加</h2>
-            <h2>{{ $month }}</h2>
+            <h2>{{ \Illuminate\Support\Facades\Auth::user()->id }}</h2>
             <form action="{{ route('data.create') }}" method="post">
                 @csrf
                 <div class="form-group">
