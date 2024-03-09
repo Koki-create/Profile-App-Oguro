02c57d8a02cca8310daaee3b2d1561536c401aaf
Koki <wamauup.h.h.h@gmail.com>
2024-03-10 04:04:53 +0900

    My Portfolioにhrefをつけて画面遷移可能に

--- resources/views/user/sign-up.blade.php
+++ resources/views/user/sign-up.blade.php
@@ -11,7 +11,7 @@
 <body>
     <header>
         <div class="header-content">
-            <h1>My Portfolio</h1>
+            <h1><a href="route('user.top')" style="color: white; text-decoration: none;">My Portfolio</a></h1>
         </div>
         <form action="{{ route('user.showLogin') }}" method="get">
             <button type="submit" class="button_white">ログイン</button>
